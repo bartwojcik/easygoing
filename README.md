b05a1b0cce0c2e022dadc8b760a3a0762f0cea3a
Bartosz Wojcik <bartwojc@gmail.com>
2017-05-09 00:32:22 +0200

    WIP safety commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Easygoing - a simple, easy and secure personal blog.
+## TODOs:
+- Automate deploy.
+  - Generate nginx config from template.
+  - Generate docker-compose file from template.
+  - Add certbot / obtain certificates through certbot.
+  - Ensure certbot runs so that certificate can update automatically (saved on a volume).
+- Ensure robots.txt is present and site can be crawled (should this be a setting?).
+- Simple file upload (same as image?) service (do not use DB).
+- Configurable background (option to upload image(s)?).
+- Configurable styles option (bootstrap css?) (with option to upload css?).
+- Customize bootstrap?
+- Add licenses view under /license url to comply with various licenses.
+- Add search (haystack?).
+
+## License
+(License is yet to be chosen.)
+
+Copyright Bartosz Wójcik <bartwojc@gmail.com>
--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,44 @@
+import random
+
 from fabric.context_managers import settings
+from fabric.contrib.files import upload_template
 from fabric.decorators import task
 from fabric.operations import run, local, put
+from fabric.state import env
+
+random = random.SystemRandom()
+
+
+def get_random_string(length=12,
+                      allowed_chars='abcdefghijklmnopqrstuvwxyz'
+                                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    """
+    Returns a securely generated random string.
+ 
+    The default length of 12 with the a-z, A-Z, 0-9 character set returns
+    a 71-bit value. log_2((26+26+10)^12) =~ 71 bits.
+ 
+    Taken from the django.utils.crypto module.
+    """
+    return ''.join(random.choice(allowed_chars) for _ in range(length))
+
+
+def get_secret_key():
+    """
+    Create a random secret key.
+ 
+    Taken from the Django project.
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    return get_random_string(50, chars)
+
+
+def escape_docker_strings(string):
+    return string.replace('$', '$$')
 
 
 @task
-def install_docker_debian(skip_group_assignment=False):
+def install_docker_debian():
     with settings(warn_only=True):
         run('apt-get remove docker docker-engine')
     with settings(warn_only=True):
@@ -20,7 +54,7 @@ def install_docker_debian(skip_group_assignment=False):
             'stable"')
         run('apt-get update -y')
         run('apt-get install -y docker-ce')
-        # TODO change when (if?) docker-compose is available in debian repo
+        # TODO rewrite when (if?) docker-compose is available in debian repo
         run(
             'curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"'
             ' -o /usr/local/bin/docker-compose')
@@ -31,42 +65,58 @@ def install_docker_debian(skip_group_assignment=False):
 
 @task
 def build():
+    docker_running = local('systemctl is-active docker')
+    if docker_running == 'inactive':
+        local('sudo systemctl start docker')
     local('docker-compose -f docker-compose.yml build')
 
 
 @task
 def upload():
-    # TODO check if docker compose bundle is appropriate instead of copying docker-compose file
+    # TODO check if docker compose bundle is appropriate instead of copying docker-compose files
     local('docker image save easygoing -o easygoing.image')
     run('mkdir -p ~/easygoing')
     put(local_path='docker-compose.yml', remote_path='~/easygoing/docker-compose.yml')
     put(local_path='easygoing.image', remote_path='~/easygoing/easygoing.image')
     local('rm easygoing.image')
-    run('docker image import ~/easygoing/easygoing.image easygoing:latest')
+    run('docker image load -i ~/easygoing/easygoing.image')
     run('rm ~/easygoing/easygoing.image')
+
+
+@task
+def setup():
+    context = {'fqdn': env.host,
+               'secret_key': escape_docker_strings(get_secret_key())}
+    upload_template(filename='nginx.conf', destination='~/easygoing/nginx.conf', context=context, use_jinja=True)
+    upload_template(filename='docker-compose.prod.yml', destination='~/easygoing/docker-compose.prod.yml',
+                    context=context, use_jinja=True)
+    with settings(warn_only=True):
+        run('docker rm helper')
+    run('docker run -v easygoing_nginx_conf:/data --name helper busybox true')
+    run('docker cp ~/easygoing/nginx.conf helper:/data/sites-available/easygoing')
+    run('docker run ')  # TODO tu skonczylem - add proper nginx configuration
+    run('docker rm helper')
+    script = '''
+    mkdir -p /var/easygoing/logs &&
+    touch /var/easygoing/logs/gunicorn.log &&
+    touch /var/easygoing/logs/nginx-access.log &&
+    touch /var/easygoing/logs/nginx-error.log &&
+    chown -R user:user /var/easygoing &&
+    python wait_for.py db 5432 &&
+    python wait_for.py cache 6379 &&
+    python manage.py migrate --noinput &&
+    python manage.py collectstatic --noinput
+    '''
+    run("docker-compose -f ~/easygoing/docker-compose.yml run gunicorn /bin/bash -c '{}'".format(script))
 
 
 @task
 def deploy_production():
     build()
     install_docker_debian()
-    # install_socat_debian()
     upload()
-    # TODO tu skonczylem
-    script = '''
-    gunicorn mkdir -p /var/easygoing/logs &&
-    touch /var/easygoing/logs/gunicorn.log &&
-    touch /var/easygoing/logs/nginx-access.log &&
-    touch /var/easygoing/logs/nginx-error.log &&
-    chown -R user:user /var/easygoing &&
-    python wait_for.py db 5432 &&
-    python wait_for.py cache &&
-    python manage.py migrate &&
-    python manage.py collectstatic
-    '''
-    run("docker-compose run -u root gunicorn {}".format(script))
-
-
+    setup()
+    clear()
 
 
 @task
@@ -78,19 +128,19 @@ def disable_webserver():
 @task
 def up():
     disable_webserver()
-    run('/usr/local/bin/docker-compose -f ~/easygoing/docker-compose.yml up -d')
+    run('/usr/local/bin/docker-compose -f ~/easygoing/docker-compose.yml -f ~/easygoing/docker-compose.prod.yml up -d')
 
 
 @task
 def down():
-    run('docker-compose -f ~/easygoing/docker-compose.yml down')
+    run('docker-compose -f ~/easygoing/docker-compose.yml -f ~/easygoing/docker-compose.prod.yml down')
 
 
 @task
-def remove():
-    down()
-    run('docker image rm easygoing')
-    run('rm -rf ~/easygoing')
+def clear():
+    run('rm -f ~/easygoing/easygoing.image')
+    local('rm -f easygoing.image')
+
 
 @task
 def uninstall_docker_debian():
@@ -100,14 +150,18 @@ def uninstall_docker_debian():
     run('systemctl disable docker')
     run('systemctl stop docker')
     run('apt-get uninstall -y docker-ce')
-    run('rm /usr/local/bin/docker-compose')
+    run('rm -f /usr/local/bin/docker-compose')
     run('add-apt-repository -r -y'
         '"deb [arch=amd64] https://download.docker.com/linux/debian '
         '$(lsb_release -cs) '
         'stable"')
 
+
 @task
-def clean():
-    down()
-    remove()
+def remove():
+    with settings(warn_only=True):
+        down()
+        clear()
+    run('docker image rm -f easygoing')
+    run('rm -rf ~/easygoing')
     uninstall_docker_debian()

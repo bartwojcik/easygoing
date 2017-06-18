3724891a068e3adc6fe75e342faba2926346b675
Bartosz Wojcik <bartwojc@gmail.com>
2017-06-18 21:49:01 +0200

    Add missing models to admin.py

--- stem/admin.py
+++ stem/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from stem.models import Website, Post, Comment
+from stem.models import Website, Post, Comment, UploadedFile
 
 admin.site.register(Website)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(UploadedFile)
